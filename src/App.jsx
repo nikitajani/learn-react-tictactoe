419dac420d3141c8dadc1ac3359ab2917f58e4e3
Nikita Jani <nikitanehaj492@gmail.com>
2024-06-28 22:38:08 +0530

    feat: Calculating winner

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,40 @@ function App() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  const calculateWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = calculateWinner();
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  }
+
   const handleClick = (i) => {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner()) {
       return;
     }
     const nextSqaures = squares.slice();
@@ -19,8 +51,10 @@ function App() {
     setSquares(nextSqaures);
     setXIsNext(!xIsNext);
   };
+
   return (
     <>
+      <div className="winner">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
